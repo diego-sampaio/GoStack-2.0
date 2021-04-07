338dc7cfee9323e7eebcc2c29e53eec4b731e0d6
Diego da Silva Sampaio <diego.sampaio@go.senac.br>
2021-04-06 23:05:51 -0300

    GoBarber API

---
+++ backend/src/routes.js
@@ -0,0 +1,44 @@
+import { Router } from 'express';
+
+import multer from 'multer';
+import multerConfig from './config/multer';
+
+import SessionController from './app/controllers/SessionController';
+
+import authMiddleware from './app/middlewares/auth';
+
+import UserController from './app/controllers/UserController';
+import ProviderController from './app/controllers/ProviderController';
+import FileController from './app/controllers/FileController';
+import AppointmentController from './app/controllers/AppointmentController';
+import ScheduleController from './app/controllers/ScheduleController';
+import NotificationController from './app/controllers/NotificationController';
+import AvailableController from './app/controllers/AvailableController';
+
+const routes = new Router();
+const upload = multer(multerConfig);
+
+routes.post('/sessions', SessionController.store);
+
+routes.post('/users', UserController.store);
+
+// The 'authMiddleware' must be used before all the routes that needs this authentication middleware.
+routes.use(authMiddleware);
+
+routes.put('/users', UserController.update);
+
+routes.get('/providers', ProviderController.index);
+routes.get('/providers/:providerId/available', AvailableController.index);
+
+routes.post('/files', upload.single('file'), FileController.store);
+
+routes.get('/appointments', AppointmentController.index);
+routes.post('/appointments', AppointmentController.store);
+routes.delete('/appointments/:id', AppointmentController.delete);
+
+routes.get('/schedule', ScheduleController.index);
+
+routes.get('/notifications', NotificationController.index);
+routes.put('/notifications/:id', NotificationController.update);
+
+export default routes;
